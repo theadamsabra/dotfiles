f20d790b45681dc1913cfcb0be2b10b70048428a
Adam Sabra <theadamsabra@gmail.com>
2024-06-25 16:44:56 -0700

    add nvim config

---
+++ .config/nvim/lua/vim-config.lua
@@ -0,0 +1,11 @@
+-- Fix up some tabs:
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Set leader:
+vim.g.mapleader = " "
+
+-- Relative line numbers:
+vim.wo.relativenumber = true
