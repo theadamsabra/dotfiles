16374c4fc8687dda22b1ef0e783516a0ff5c0aa2
Adam Sabra <theadamsabra@gmail.com>
2024-06-25 19:58:16 -0700

    show hidden files in neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,18 @@ return {
       "MunifTanjim/nui.nvim",
       },
     config = function()
+      require("neo-tree").setup({
+          filesystem = {
+              filtered_items = {
+                  visible = true,
+                  show_hidden_count = true,
+                  hide_dotfiles = false,
+                  hide_gitignored = true,
+                  hide_by_name = {},
+                  never_show = {},
+              },
+          },
+      })
       vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
     end
   }
