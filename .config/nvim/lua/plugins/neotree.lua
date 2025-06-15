ef43b06795d16660d77b5930658e47ac5ae23230
theadamsabra <theadamsabra@gmail.com>
2025-06-15 15:08:24 -0700

    finally update this

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -20,5 +20,6 @@ return {
           },
       })
       vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+      vim.keymap.set('n', '<C-b>', ':Neotree filesystem close<CR>')
     end
   }
--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -1,11 +1,15 @@
--- Fix up some tabs:
+-- Setup some stuff 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set cursorline")
+
+vim.cmd("nnoremap <c>d <c>dzz")
+vim.cmd("nnoremap <c>u <c>uzz")
 
 -- Set leader:
 vim.g.mapleader = " "
 
 -- Relative line numbers:
-vim.wo.relativenumber = true
+vim.opt.relativenumber = true
