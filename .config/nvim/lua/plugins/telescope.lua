221ef6e694c29101e697ab9af5216553f876a206
theadamsabra <theadamsabra@gmail.com>
2025-06-15 15:26:49 -0700

    finally telescope works for hidden dirs lmao

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,36 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+    -- Allow for hidden files
+    -- https://stackoverflow.com/questions/71809098/how-to-include-specific-hidden-file-folder-in-search-result-when-using-telescope/76991432#76991432
+    require('telescope').setup({
+      defaults = {
+        -- configure to use ripgrep
+        vimgrep_arguments = {
+          "rg",
+          "--follow",        -- Follow symbolic links
+          "--hidden",        -- Search for hidden files
+          "--no-heading",    -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number",   -- Show line numbers
+          "--column",        -- Show column numbers
+          "--smart-case",    -- Smart case search
+          -- Exclude some patterns from search
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      }
+    })
   end
 }
