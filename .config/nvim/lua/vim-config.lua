05dabe297e57b70df46c7fe0823d1a6f93492bd4
theadamsabra <theadamsabra@gmail.com>
2025-06-15 15:16:48 -0700

    this doesn't work yet

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -5,9 +5,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set cursorline")
 
-vim.cmd("nnoremap <c>d <c>dzz")
-vim.cmd("nnoremap <c>u <c>uzz")
-
 -- Set leader:
 vim.g.mapleader = " "
 
