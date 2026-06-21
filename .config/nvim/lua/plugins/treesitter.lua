78d75613e743ddea6667d7396a02374b9a631dab
Adam Sabra <theadamsabra@gmail.com>
2026-06-21 08:36:55 -0700

    fix treesitter error

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "python", "markdown"},
-      highlight = {enable = true},
-      indent = {enable = true}
-    })
-  end
+  opts = {
+    ensure_installed = {"lua", "python", "markdown"},
+    highlight = {enable = true},
+    indent = {enable = true}
+  }
 }
